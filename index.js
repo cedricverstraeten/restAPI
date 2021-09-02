e27745b3ae64b3b1d36aab3412ed0610addb238b
Cedric <44072489+cedricverstraeten@users.noreply.github.com>
2021-09-02 21:01:47 +0200

    Moved api
    All api methods are to be called from routes/api

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const bodyParser = require("body-parser")
 const jwt = require('jsonwebtoken')
-//const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const passport = require('passport');
-const Joi = require('joi');
+
 const path = require('path');
 const fs = require('fs')
 const app = express()
@@ -19,23 +19,9 @@ const { abort } = require('process');
 
 // parses json data sent to us by the user 
 app.use(bodyParser.json());
-//app.use('//api', require ('./routes//api'));
+app.use('/api', require ('./routes/api'));
 
-//Create controlschemas
-const controlSchemaMaterial = Joi.object().keys({
-    name : Joi.string().required(),
-    type : Joi.string().required(),
-    createdBy : Joi.string().required(),
-    isBorrowed : Joi.boolean().required(),
-    isDeleted : Joi.boolean().required()
-});
-const controlSchemaUser = Joi.object().keys({
-    Name : Joi.string().required(),
-    Password : Joi.string().required(),
-    Mail : Joi.string().required(),
-    admin: Joi.boolean().required(),
-    isDeleted: Joi.boolean().required()
-});
+
 
 
 //------------------------------------------
@@ -43,175 +29,12 @@ const controlSchemaUser = Joi.object().keys({
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // OOK IN /api WAARDE TOEVOEGEN
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+    res.header("Access-Control-Allow-Methods", "POST, GET");
     res.header("X-Content-Type-Options", "nosniff");
     next();
   });
 //--------------------------------------------------
 
-// Get all items of a given collection
-app.get('/api/:collection', async (req,res)=>{
-
-    //if(req.header.Access-Control-Allow-Origin == null || req.header.Access-Control-Allow-Origin == undefined)
-    //Get collection name
-    const collection = req.params.collection
-
-    //retrieve data
-    await db.getDB()
-        .collection(collection)
-        .find({})
-        .toArray((err,collection)=>{
-            if(err){
-                console.log(err);
-                res
-                    .status(404)
-                    .end()
-            } else {
-                //res.status=200
-                res
-                    .status(200)
-                    .type('application/json')
-                    .json(collection);
-            }
-        });
-});
-
-// Get item by ID of a given collection 
-app.get('/api/:collection/:id', isAuthorized, async (req,res)=>{
-    //Get collection name & itemID
-    const collection = req.params.collection
-    const itemID = ObjectId(req.params.id)
-    //const itemID = db.getPrimaryKey(req.params.id)
-
-    //get item of collection
-    await db.getDB()
-        .collection(collection)
-        .find({_id:itemID})
-        .toArray((err,item)=>{
-            if(err){
-                console.log(err);
-                res
-                    .status(404)
-                    .end()
-            } else {
-                res
-                    .status(200)
-                    .type('application/json')
-                    .json(item);
-            }
-        });
-});
-
-//Set options voor collections
-app.options('/api/:collection', isAuthorized, (req, res)=> {
-    res
-        .status(204)
-        .header('Allow','OPTIONS, GET, POST')
-        .end()
-});
-
-//Set options voor items in collections
-app.options('/api/:collection/:id', isAuthorized, (req, res)=> {
-    res
-        .status(204)
-        .header('Allow','OPTIONS, DELETE, GET, PUT')
-        .end()
-});
-
-//Create new item
-app.post('/api/:collection', isAuthorized, async (req, res)=>{
-    //get collection name
-    const collection = req.params.collection
-
-    //Determine wich object is passed and return appropiated schema
-    const schema = selectSchema(collection, req.body)
-
-    await db
-        .getDB()
-        .collection(collection)
-        .insertOne(schema, (err,item)=>{
-            if (err) {
-                console.log("[Error]: " + err)
-                res
-                    .status(400)
-                    .json({'message': "Item could not be created."})
-            } else{
-                console.log(item)
-                res
-                    .status(201)
-                    .json({"message":"Item " + schema.ObjectId + " created!"})
-            }
-        })
-});
-
-//Delete item
-app.delete('/api/:collection/:id', isAuthorized, async (req, res) =>{
-    //Get collection name & itemID
-    const collection = req.params.collection
-    const itemID = ObjectId(req.params.id)
-    //const itemID = db.getPrimaryKey(req.params.id)
-
-    await db
-        .getDB()
-        .collection(collection)
-        .findOneAndUpdate({_id:itemID}, {$set : {isDeleted : true}}, {returnOriginal : false}, (err, result)=> {
-            if(err){
-                console.log(err);
-                res
-                    .status(404)
-                    .end()
-            } else {
-                res
-                    .status(204)
-                    .json({"message":"Item " + itemID + " deleted!"});
-            }
-        })
-});
-
-//Update item
-app.put('/api/:collection/:id', isAuthorized, async (req, res)=>{
-    //Get collection name & itemID
-    const collection = req.params.collection
-    const itemID = ObjectId(req.params.id)
-    //const itemID = db.getPrimaryKey(req.params.id)
-
-    //Instead of updating the document, replace it because:
-    //(1) collection at runtime is unknow, so how to update the document? 
-    //(2) when creating a new schema, a new id is generated. This defeats the point of updating where the id is kept
-    await db
-        .getDB()
-        .collection(collection)
-        .replaceOne({_id:itemID}, req.body, upsert = false, (err, result)=> {
-            if(err){
-                console.log(err);
-                res
-                    .status(404)
-                    .end()
-            } else {
-                res
-                    .status(204)
-                    .json({"message":"Item " + itemID + " updated!"});
-            }
-        })  
-});
-
-//Following reqests are not approved (status 405):
-//collection: PUT, PATCH, DELETE
-app.put('/api/:collection', async (req, res)=>{
-    res
-        .status(405)
-        .end()
-});
-app.patch('/api/:collection', async (req, res)=>{
-    res
-        .status(405)
-        .end()
-});
-app.delete('/api/:collection', async (req, res)=>{
-    res
-        .status(405)
-        .end()
-});
 
 
 app.post('/login', async (req, res) =>{
@@ -227,26 +50,36 @@ app.post('/login', async (req, res) =>{
                     .status(404)
                     .end()
             } else {
-                //res.status=200
-                // res
-                //     .status(200)
-                //     .type('application/json')
-                //     .json(collection);
 
-                console.log("Found user:" + login);
-                if (login>0) {
-                    if (login.Password === req.body.Password) {
-                        let privateKey = fs.readFileSync('./private.pem', 'utf8');
-                        let token = jwt.sign({"body":"stuff"}, privateKey, {algorithm: 'HS256'});
+                //console.log(login.count < 0)    
+                // console.log(login[0].Mail)
+                // console.log(login<0)
+                if(login == 0 ){
+                    res
+                        .status(404)
+                        .end()
+                } else {
+                    //console.log(login)
+                    if (login[0].Password === req.body.Password) {
                         res
                             .status(200)
-                            .send(token);
+                            .end()
+                        
+                    } else {
+                        res
+                            .status(404)
+                            .end()
                     }
-                } else {
-                    res.redirect('/')
                 }
+
+
+                // res.status=200
+                // res
+                //     //.status(200)
+                //     .type('application/json')
+                //     .json(login);
             }
-        });
+        })
 });
 //app.get('/', (req, res) => res.send("Server is running"))
 
@@ -261,6 +94,50 @@ app.get('/jwt', (req, res) =>{
     res.send(token);
 
 })
+
+app.post('/test', async (req, res) => {
+    //retrieve data
+    await db.getDB()
+        .collection('Users')
+        .find({"Mail":req.body.Mail})
+        .toArray((err,login)=>{
+            if(err){
+                console.log(err);
+                res
+                    .status(404)
+                    .end()
+            } else {
+
+                //console.log(login.count < 0)    
+                // console.log(login[0].Mail)
+                // console.log(login<0)
+                if(login == 0 ){
+                    res
+                        .status(404)
+                        .end()
+                } else {
+                    //console.log(login)
+                    if (login[0].Password === req.body.Password) {
+                        res
+                            .status(200)
+                            .end()
+                        
+                    } else {
+                        res
+                            .status(404)
+                            .end()
+                    }
+                }
+
+
+                // res.status=200
+                // res
+                //     //.status(200)
+                //     .type('application/json')
+                //     .json(login);
+            }
+        })
+});
 
 
 function isAuthorized(req, res, next){
@@ -285,83 +162,7 @@ function isAuthorized(req, res, next){
     }
 }
 
-function selectSchema(collection, reqBody){
-    //console.log(reqBody)
 
-    if(collection == "Users"){
-        const newUser = new User;
-        
-        //const hashedPassword = bcrypt.hash(reqBody.Password, 12);
-
-        newUser.Name = reqBody.Name;
-        newUser.Password = reqBody.Password;
-        newUser.Mail = reqBody.Mail;
-        newUser.BorrowedItems = reqBody.BorrowedItems;
-        newUser.admin = reqBody.admin;
-        newUser.isDeleted = reqBody.isDeleted;
-        
-        console.log("User: " + newUser)
-
-        return newUser;
-    } else {
-        const newMaterial = new Material;
-
-        newMaterial.name = reqBody.name;
-        newMaterial.type = reqBody.type;
-        newMaterial.createdBy = reqBody.createdBy;
-        newMaterial.isBorrowed = reqBody.isBorrowed;
-        newMaterial.isDeleted = reqBody.isDeleted;
-
-        console.log("Material: " + newMaterial)
-
-        return newMaterial;
-    }
-
-    /*
-    
-
-    if(collection == "Users"){
-        reqBody.assert('Name', 'Name is required').notEmpty() //Validate name
-        reqBody.assert('Password', 'Password is required').notEmpty() //Validate Password
-        reqBody.assert('Mail', 'A valid email is required').isEmail() //Validate email
-        
-        var errors = req.validationErrors()
-        if (!errors) { 
-            //No errors were found.  Passed Validation!
-            newUser.Name = reqBody.Name;
-            newUser.Password = reqBody.Password;
-            newUser.Mail = reqBody.Mail;
-            newUser.BorrowedItems = reqBody.BorrowedItems;
-            newUser.admin = reqBody.admin;
-            newUser.isDeleted = reqBody.isDeleted;
-            
-            console.log("User: " + newUser)
-
-            return newUser;
-        }
-    } else {
-
-        reqBody.assert('name', 'Name is required').notEmpty() //Validate name
-        reqBody.assert('type', 'type is required').notEmpty() //Validate type
-        reqBody.assert('createdBy', 'createdBy is required').notEmpty() //Validate createdBy
-        
-        var errors = req.validationErrors()
-        if (!errors) { 
-            const newMaterial = new Material;
-
-            newMaterial.name = reqBody.name;
-            newMaterial.type = reqBody.type;
-            newMaterial.createdBy = reqBody.createdBy;
-            newMaterial.isBorrowed = reqBody.isBorrowed;
-            newMaterial.isDeleted = reqBody.isDeleted;
-
-            console.log("Material: " + newMaterial)
-
-            return newMaterial;
-        }
-    }
-    */
-}
 
 db.connect((err)=>{
     // If err unable to connect to database
--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 //const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken')
+const Joi = require('joi');
+const fs = require('fs')
 
 const db = require('../database/mongo');
 const { ObjectId } = require('mongodb');
@@ -10,9 +12,40 @@ const { ObjectId } = require('mongodb');
 const Material = require('../models/Material')
 
 
-// Get all items of a given collection
-app.get('/:collection', isAuthorized, async (req,res)=>{
+//Create controlschemas
+const controlSchemaMaterial = Joi.object().keys({
+    name : Joi.string().required(),
+    type : Joi.string().required(),
+    createdBy : Joi.string().required(),
+    isBorrowed : Joi.boolean().required(),
+    isDeleted : Joi.boolean().required()
+});
+const controlSchemaUser = Joi.object().keys({
+    Name : Joi.string().required(),
+    Password : Joi.string().required(),
+    Mail : Joi.string().required(),
+    admin: Joi.boolean().required(),
+    isDeleted: Joi.boolean().required()
+});
 
+
+//------------------------------------------
+//Allow all requests from all domains & localhost
+router.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // OOK IN /api WAARDE TOEVOEGEN
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+    res.header("X-Content-Type-Options", "nosniff");
+    next();
+  });
+//--------------------------------------------------
+
+
+
+// Get all items of a given collection
+router.get('/:collection', async (req,res)=>{
+
+    //if(req.header.Access-Control-Allow-Origin == null || req.header.Access-Control-Allow-Origin == undefined)
     //Get collection name
     const collection = req.params.collection
 
@@ -21,10 +54,15 @@ app.get('/:collection', isAuthorized, async (req,res)=>{
         .collection(collection)
         .find({})
         .toArray((err,collection)=>{
-            if(err)
+            if(err){
                 console.log(err);
-            else{
                 res
+                    .status(404)
+                    .end()
+            } else {
+                //res.status=200
+                res
+                    .status(200)
                     .type('application/json')
                     .json(collection);
             }
@@ -32,20 +70,25 @@ app.get('/:collection', isAuthorized, async (req,res)=>{
 });
 
 // Get item by ID of a given collection 
-app.get('/:collection/:id', isAuthorized, async (req,res)=>{
-    //Get collection name
+router.get('/:collection/:id', isAuthorized, async (req,res)=>{
+    //Get collection name & itemID
     const collection = req.params.collection
     const itemID = ObjectId(req.params.id)
+    //const itemID = db.getPrimaryKey(req.params.id)
 
     //get item of collection
     await db.getDB()
         .collection(collection)
         .find({_id:itemID})
         .toArray((err,item)=>{
-            if(err)
+            if(err){
                 console.log(err);
-            else{
                 res
+                    .status(404)
+                    .end()
+            } else {
+                res
+                    .status(200)
                     .type('application/json')
                     .json(item);
             }
@@ -53,29 +96,117 @@ app.get('/:collection/:id', isAuthorized, async (req,res)=>{
 });
 
 //Set options voor collections
-app.options('/:collection', isAuthorized, (req, res)=> {
+router.options('/:collection', isAuthorized, (req, res)=> {
     res
-        .header('Allow','OPTIONS, GET')
+        .status(204)
+        .header('Allow','OPTIONS, GET, POST')
         .end()
 });
-
-
 
 //Set options voor items in collections
-app.options('/:collection/:id', isAuthorized, (req, res)=> {
+router.options('/:collection/:id', isAuthorized, (req, res)=> {
     res
-        .header('Allow','OPTIONS, GET')
+        .status(204)
+        .header('Allow','OPTIONS, DELETE, GET, PUT')
         .end()
 });
 
-app.get('/', (req, res) => res.send("Server is running"))
+//Create new item
+router.post('/:collection', isAuthorized, async (req, res)=>{
+    //get collection name
+    const collection = req.params.collection
 
-app.get('/secret', isAuthorized, (req, res) => {
-    res.json({"message": "Super Secret Message"});
-})
+    //Determine wich object is passed and return appropiated schema
+    const schema = selectSchema(collection, req.body)
 
+    await db
+        .getDB()
+        .collection(collection)
+        .insertOne(schema, (err,item)=>{
+            if (err) {
+                console.log("[Error]: " + err)
+                res
+                    .status(400)
+                    .json({'message': "Item could not be created."})
+            } else{
+                console.log(item)
+                res
+                    .status(201)
+                    .json({"message":"Item " + schema.ObjectId + " created!"})
+            }
+        })
+});
 
-app.get('/jwt', (req, res) =>{
+//Delete item
+router.delete('/:collection/:id', isAuthorized, async (req, res) =>{
+    //Get collection name & itemID
+    const collection = req.params.collection
+    const itemID = ObjectId(req.params.id)
+    //const itemID = db.getPrimaryKey(req.params.id)
+
+    await db
+        .getDB()
+        .collection(collection)
+        .findOneAndUpdate({_id:itemID}, {$set : {isDeleted : true}}, {returnOriginal : false}, (err, result)=> {
+            if(err){
+                console.log(err);
+                res
+                    .status(404)
+                    .end()
+            } else {
+                res
+                    .status(204)
+                    .json({"message":"Item " + itemID + " deleted!"});
+            }
+        })
+});
+
+//Update item
+router.put('/:collection/:id', isAuthorized, async (req, res)=>{
+    //Get collection name & itemID
+    const collection = req.params.collection
+    const itemID = ObjectId(req.params.id)
+    //const itemID = db.getPrimaryKey(req.params.id)
+
+    //Instead of updating the document, replace it because:
+    //(1) collection at runtime is unknow, so how to update the document? 
+    //(2) when creating a new schema, a new id is generated. This defeats the point of updating where the id is kept
+    await db
+        .getDB()
+        .collection(collection)
+        .replaceOne({_id:itemID}, req.body, upsert = false, (err, result)=> {
+            if(err){
+                console.log(err);
+                res
+                    .status(404)
+                    .end()
+            } else {
+                res
+                    .status(204)
+                    .json({"message":"Item " + itemID + " updated!"});
+            }
+        })  
+});
+
+//Following reqests are not approved (status 405):
+//collection: PUT, PATCH, DELETE
+router.put('/:collection', async (req, res)=>{
+    res
+        .status(405)
+        .end()
+});
+router.patch('/:collection', async (req, res)=>{
+    res
+        .status(405)
+        .end()
+});
+router.delete('/:collection', async (req, res)=>{
+    res
+        .status(405)
+        .end()
+});
+
+router.get('/jwt', (req, res) =>{
     let privateKey = fs.readFileSync('./private.pem', 'utf8');
     let token = jwt.sign({"body":"stuff"}, privateKey, {algorithm: 'HS256'});
     res.send(token);
@@ -108,17 +239,97 @@ function isAuthorized(req, res, next){
 }
 
 
-db.connect((err)=>{
-    // If err unable to connect to database
-    // End application
-    if(err){
-        console.log('unable to connect to database');
-        process.exit(1);
+function selectSchema(collection, reqBody){
+    //console.log(reqBody)
+
+    if(collection == "Users"){
+        const newUser = new User;
+        
+        const hashedPassword = bcrypt.hash(reqBody.Password, 12);
+
+        newUser.Name = reqBody.Name;
+        newUser.Password = hashedPassword;
+        newUser.Mail = reqBody.Mail;
+        newUser.BorrowedItems = reqBody.BorrowedItems;
+        newUser.admin = reqBody.admin;
+        newUser.isDeleted = reqBody.isDeleted;
+        
+        console.log("User: " + newUser)
+
+        return newUser;
+    } else {
+        const newMaterial = new Material;
+
+        newMaterial.name = reqBody.name;
+        newMaterial.type = reqBody.type;
+        newMaterial.createdBy = reqBody.createdBy;
+        newMaterial.isBorrowed = reqBody.isBorrowed;
+        newMaterial.isDeleted = reqBody.isDeleted;
+
+        console.log("Material: " + newMaterial)
+
+        return newMaterial;
     }
-    // Successfully connected to database
-    // Start up our Express Application
-    // And listen for Request
-    else{
-        app.listen(port, () => console.log(`Connected to database. Started App on port: ${port}`))
+
+    /*
+    
+
+    if(collection == "Users"){
+        reqBody.assert('Name', 'Name is required').notEmpty() //Validate name
+        reqBody.assert('Password', 'Password is required').notEmpty() //Validate Password
+        reqBody.assert('Mail', 'A valid email is required').isEmail() //Validate email
+        
+        var errors = req.validationErrors()
+        if (!errors) { 
+            //No errors were found.  Passed Validation!
+            newUser.Name = reqBody.Name;
+            newUser.Password = reqBody.Password;
+            newUser.Mail = reqBody.Mail;
+            newUser.BorrowedItems = reqBody.BorrowedItems;
+            newUser.admin = reqBody.admin;
+            newUser.isDeleted = reqBody.isDeleted;
+            
+            console.log("User: " + newUser)
+
+            return newUser;
+        }
+    } else {
+
+        reqBody.assert('name', 'Name is required').notEmpty() //Validate name
+        reqBody.assert('type', 'type is required').notEmpty() //Validate type
+        reqBody.assert('createdBy', 'createdBy is required').notEmpty() //Validate createdBy
+        
+        var errors = req.validationErrors()
+        if (!errors) { 
+            const newMaterial = new Material;
+
+            newMaterial.name = reqBody.name;
+            newMaterial.type = reqBody.type;
+            newMaterial.createdBy = reqBody.createdBy;
+            newMaterial.isBorrowed = reqBody.isBorrowed;
+            newMaterial.isDeleted = reqBody.isDeleted;
+
+            console.log("Material: " + newMaterial)
+
+            return newMaterial;
+        }
     }
-});
+    */
+}
+
+// db.connect((err)=>{
+//     // If err unable to connect to database
+//     // End application
+//     if(err){
+//         console.log('unable to connect to database');
+//         process.exit(1);
+//     }
+//     // Successfully connected to database
+//     // Start up our Express Application
+//     // And listen for Request
+//     else{
+//         router.listen(port, () => console.log(`Connected to database. Started router on port: ${port}`))
+//     }
+// });
+
+module.exports = router
