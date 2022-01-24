103be250341d0a90e7827da5fc2bd2e7e3d9b637
Cedric <44072489+cedricverstraeten@users.noreply.github.com>
2022-01-24 21:53:55 +0100

    Access-Control-Allow-Headers disabled

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use('/api', require ('./routes/api'));
 app.all('/*', function(req, res, next) {
     //res.header("Access-Control-Allow-Origin", "https://myfablab.herokuapp.com"); // OOK IN /api WAARDE TOEVOEGEN
     res.header("Access-Control-Allow-Origin", "*"); 
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+    //res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET");
     res.header("X-Content-Type-Options", "nosniff");
     res.header("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload")
