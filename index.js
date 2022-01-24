568b18ec171a005539e007a9b0728087b5e55060
Cedric <44072489+cedricverstraeten@users.noreply.github.com>
2022-01-24 22:05:13 +0100

    all headers disabled

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ app.use('/api', require ('./routes/api'));
 //Allow all requests from all domains & localhost
 app.all('/*', function(req, res, next) {
     //res.header("Access-Control-Allow-Origin", "https://myfablab.herokuapp.com"); // OOK IN /api WAARDE TOEVOEGEN
-    res.header("Access-Control-Allow-Origin", "*"); 
+    //res.header("Access-Control-Allow-Origin", "*"); 
     //res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET");
-    res.header("X-Content-Type-Options", "nosniff");
+    //res.header("Access-Control-Allow-Methods", "POST, GET");
+    //res.header("X-Content-Type-Options", "nosniff");
     //res.header("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload")
     next();
   });
